e7f0935a248488a4277eb5847d58899738cd9fc5
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-03-08 00:24:12 -0500

    fix random image gen

--- components/SamilaImage.tsx
+++ components/SamilaImage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 
 type ImageProps = {
@@ -8,15 +9,22 @@ function getRandomInt(max: number): string {
     return Math.floor(Math.random() * max).toString();
 }
 
-function randomImage(): string {
+function getRandomImage(): string {
     const BASE_PATH = "/images/example_";
-    return BASE_PATH + getRandomInt(6) + ".png";
+    return BASE_PATH + getRandomInt(4) + ".png";
 }
 
 export default function SamilaImage({ img }: ImageProps) {
+    const [placeholder, setPlaceholder] = useState(getRandomImage);
+    useEffect(() => {
+        const i = getRandomImage();
+        setPlaceholder(i);
+        console.log(i);
+    }, []);
+
     return (
         <div className="relative flex flex-col items-center justify-center m-5 lg:mt-8 drop-shadow">
-            {!img && <Image src={randomImage()} alt="plot" width="700" height="700" />}
+            {!img && <Image src={placeholder} alt="plot" width="700" height="700" />}
             {img && <Image src={img} alt="plot" width={700} height={700} />}
         </div>
     );
